57a478c6babf484d1ed4a9f8f8c24461a3d864ae
TBchip <bischoffthijs@gmail.com>
2020-12-30 18:33:15 +0100

    change mainrobot startThreads + component disabling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/MainRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/MainRobot.java
@@ -5,6 +5,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Disabled
 public class MainRobot {
     //hardwareMap and telemetry
@@ -16,17 +20,23 @@ public class MainRobot {
     public Shooter shooter;
     public Gyroscope gyroscope;
 
-    public MainRobot(HardwareMap hardwareMap, Telemetry inputTelemetry) throws InterruptedException {
+    public ArrayList<RobotComponent> components;
+
+    public MainRobot(HardwareMap hardwareMap, Telemetry inputTelemetry, String[] inputDisabledComponents) throws InterruptedException {
         telemetry = inputTelemetry;
 
-        gyroscope = new Gyroscope(hardwareMap, telemetry, this);
-        driving = new Driving(hardwareMap, telemetry, this);
-        shooter = new Shooter(hardwareMap, telemetry, this);
+        List<String> disabledComponents = Arrays.asList(inputDisabledComponents);
+        if(!disabledComponents.contains("gyroscope"))
+            components.add( new Gyroscope(hardwareMap, telemetry, this) );
+        if(!disabledComponents.contains("driving"))
+            components.add( new Driving(hardwareMap, telemetry, this) );
+        if(!disabledComponents.contains("shooter"))
+            components.add( new Shooter(hardwareMap, telemetry, this) );
     }
 
     public void startThreats(){
-        gyroscope.startThreats();
-        driving.startThreats();
-        shooter.startThreats();
+        for(int i = 0; i < components.size(); i++){
+            components.get(i).startThreats();
+        }
     }
 }
