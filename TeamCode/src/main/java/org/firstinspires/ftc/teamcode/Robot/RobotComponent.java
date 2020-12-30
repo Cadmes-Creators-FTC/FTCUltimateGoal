522a91d9eb0441722a96c788092756c53b2dc5d3
TBchip <bischoffthijs@gmail.com>
2020-12-30 18:22:26 +0100

    made robotcomponent superclass

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotComponent.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class RobotComponent {
+    protected final Telemetry telemetry; // for logging and debugging
+    public final MainRobot robot; //reference to robot
+
+    public RobotComponent(Telemetry inputTelemetry, MainRobot inputRobot) {
+        telemetry = inputTelemetry;
+        robot = inputRobot;
+    }
+
+    public void startThreats(){
+
+    }
+}
