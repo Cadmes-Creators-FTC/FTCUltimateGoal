87c8d2416118034d140398deea533d5c3215c96a
TBchip <bischoffthijs@gmail.com>
2020-12-30 18:26:44 +0100

    made shooter extend robotcomponenet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Shooter.java
@@ -7,22 +7,20 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @Disabled
-public class Shooter {
-    private Telemetry telemetry; // for logging and debugging
-    private MainRobot robot; //reference to robot
-
+public class Shooter extends RobotComponent {
     private final DcMotor shooterWheelL;
     private final DcMotor shooterWheelR;
 
     public Shooter(HardwareMap hardwareMap, Telemetry inputTelemetry, MainRobot inputRobot) {
-        telemetry = inputTelemetry;
-        robot = inputRobot;
+        super(inputTelemetry, inputRobot);
 
         shooterWheelL = hardwareMap.get(DcMotor.class, "ShooterL");
         shooterWheelR = hardwareMap.get(DcMotor.class, "ShooterR");
 
         shooterWheelL.setDirection(DcMotor.Direction.REVERSE);
     }
+
+    @Override
     public void startThreats(){
 
     }
