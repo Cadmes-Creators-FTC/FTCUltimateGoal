9fff15f1ecb6c2a70d40e97a32887f166f8ac5f8
TBchip <bischoffthijs@gmail.com>
2020-12-30 18:59:06 +0100

    removed telemetry from robot component

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotComponent.java
@@ -5,11 +5,9 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class RobotComponent {
-    protected final Telemetry telemetry; // for logging and debugging
     public final MainRobot robot; //reference to robot
 
     public RobotComponent(Telemetry inputTelemetry, MainRobot inputRobot) {
-        telemetry = inputTelemetry;
         robot = inputRobot;
     }
 
