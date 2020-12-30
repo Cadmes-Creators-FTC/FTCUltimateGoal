1be00f7334dd02c37c692674e32d0fcac0e50d6f
TBchip <bischoffthijs@gmail.com>
2020-12-30 18:53:48 +0100

    added logging component

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Logging.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.Map;
+
+@Disabled
+public class Logging extends RobotComponent {
+    //hardwareMap and telemetry
+    private final Telemetry telemetry;
+
+    private Map<String, String> logs;
+
+    public Logging(Telemetry inputTelemetry, MainRobot inputRobot) {
+        super(inputTelemetry, inputRobot);
+
+        telemetry = inputTelemetry;
+    }
+
+    @Override
+    public void startThreads(){
+
+    }
+
+    public void setLog(String key, String value){
+        logs.put(key, value);
+    }
+    public void removeLog(String key){
+        logs.remove(key);
+    }
+
+    public void updateLogs(int delay) throws InterruptedException {
+        while (robot.isRunning){
+            for(Map.Entry<String, String> entry : logs.entrySet()){
+                telemetry.addData(entry.getKey(), entry.getValue());
+            }
+            telemetry.update();
+
+            Thread.sleep(delay);
+        }
+    }
+}
