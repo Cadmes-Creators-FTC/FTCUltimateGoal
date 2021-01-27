d92937a6c5a1c959529644e563acc763854c95de
TBchip <bischoffthijs@gmail.com>
2021-01-27 16:23:54 +0100

    added cam pipeline init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RingStackHeightDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RingStackHeightDetection.java
@@ -5,16 +5,20 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
 
 @Disabled
 public class RingStackHeightDetection extends RobotComponent {
     OpenCvInternalCamera phoneCam;
+    private final RingStackDetermenationPipeline camPipeline;
 
     public RingStackHeightDetection(HardwareMap hardwareMap, MainRobot inputRobot) {
         super(inputRobot);
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.FRONT, cameraMonitorViewId);
+        camPipeline = new RingStackDetermenationPipeline();
+        phoneCam.setPipeline(camPipeline);
     }
 
     @Override
