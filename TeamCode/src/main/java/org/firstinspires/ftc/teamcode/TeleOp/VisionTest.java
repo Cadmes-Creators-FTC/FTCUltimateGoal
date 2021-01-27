5c4a6a3f8d6fe7a29c2450d2bc34327e14174fa3
TBchip <bischoffthijs@gmail.com>
2021-01-27 16:44:15 +0100

    made VisionTest TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/VisionTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Robot.MainRobot;
+
+@TeleOp(name = "shooterOnly", group = "TestingTeleOps")
+public class VisionTest extends LinearOpMode {
+    private MainRobot robot;
+
+    @Override
+    public void runOpMode () throws InterruptedException{
+        String[] enabledComponents = {"logging", "ringStackHeightDetection"};
+        robot = new MainRobot(hardwareMap, telemetry, enabledComponents);
+
+        robot.logging.setLog("state", "Initializing");
+
+        robot.startThreads();
+
+        robot.logging.setLog("state", "Initialized, waiting for start");
+
+        waitForStart();
+
+        robot.logging.setLog("state", "Running");
+
+        while (opModeIsActive()){
+            logOrangeVal();
+        }
+
+        robot.isRunning = false;
+
+        robot.logging.setLog("state", "Stopped");
+    }
+
+    private void logOrangeVal(){
+        robot.logging.setLog("OrangeVal", robot.ringStackHeightDetection.getStackSize());
+    }
+}
