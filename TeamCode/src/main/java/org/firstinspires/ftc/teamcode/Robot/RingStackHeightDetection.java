d5f206a25d62ae777d2192c5f094c5346498ed6f
TBchip <bischoffthijs@gmail.com>
2021-01-27 15:58:44 +0100

    added RingstackHeightDetection barbone

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RingStackHeightDetection.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Disabled
+public class RingStackHeightDetection extends RobotComponent {
+
+    public RingStackHeightDetection(HardwareMap hardwareMap, MainRobot inputRobot) {
+        super(inputRobot);
+    }
+
+    @Override
+    public void startThreads(){
+
+    }
+}
