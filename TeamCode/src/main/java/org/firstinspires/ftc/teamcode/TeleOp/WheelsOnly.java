9f0131bd734277500986d17a12efd9fb1535f2c3
TBchip <bischoffthijs@gmail.com>
2021-01-27 16:48:18 +0100

    added dpad driving to driving only teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/WheelsOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/WheelsOnly.java
@@ -27,6 +27,7 @@ public class WheelsOnly extends LinearOpMode {
 
         while (opModeIsActive()){
             driveWithJoystick();
+            driveWithDpad();
         }
 
         robot.isRunning = false;
@@ -58,8 +59,47 @@ public class WheelsOnly extends LinearOpMode {
         wpc.rf = 0.6*Math.pow(wpc.rf, 3) + 0.4*wpc.rf;
         wpc.rb = 0.6*Math.pow(wpc.rb, 3) + 0.4*wpc.rb;
         wpc.lb = 0.6*Math.pow(wpc.lb, 3) + 0.4*wpc.lb;
-        robot.driving.setWheelPowers(wpc);
 
-        robot.logging.setLog("power", (wpc.lf+wpc.rf+wpc.rb+wpc.lb)/4);
+        if(wpc.lf != 0 && wpc.rf != 0 && wpc.rb != 0 && wpc.lb != 0){
+            robot.driving.setWheelPowers(wpc);
+            robot.logging.setLog("Average wheel power", (wpc.lf+wpc.rf+wpc.rb+wpc.lb)/4);
+        }
+    }
+
+    private void driveWithDpad(){
+        //remove joystick wheel power log
+        robot.logging.removeLog("Average wheel power");
+
+        boolean dpadEnabled = false;
+        WheelPowerConfig wpc = new WheelPowerConfig(0, 0, 0, 0);
+
+        if (gamepad1.dpad_up) {
+            dpadEnabled = true;
+            wpc.lf = 0.5;
+            wpc.lb = 0.5;
+            wpc.rf = 0.5;
+            wpc.rb = 0.5;
+        }else if (gamepad1.dpad_down) {
+            dpadEnabled = true;
+            wpc.lf = -0.5;
+            wpc.lb = -0.5;
+            wpc.rf = -0.5;
+            wpc.rb = -0.5;
+        }else if (gamepad1.dpad_left) {
+            dpadEnabled = true;
+            wpc.lf = -0.5;
+            wpc.lb = 0.5;
+            wpc.rf = 0.5;
+            wpc.rb = -0.5;
+        }else if (gamepad1.dpad_right){
+            dpadEnabled = true;
+            wpc.lf = 0.5;
+            wpc.lb = -0.5;
+            wpc.rf = -0.5;
+            wpc.rb = 0.5;
+        }
+
+        if(dpadEnabled)
+            robot.driving.setWheelPowers(wpc);
     }
 }
