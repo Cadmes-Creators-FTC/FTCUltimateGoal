d5aed50e56afeb62b30be87bcede4d74a9500ee1
TBchip <bischoffthijs@gmail.com>
2021-03-08 15:52:44 +0100

    removed old / slowmovement drivecontrolls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Main.java
@@ -98,7 +98,7 @@ public class Main extends LinearOpMode {
         if(!gamepad2.a)
             shooterStateChanged = false;
     }
-    double counter = 0;
+    
     boolean wobbleArmGripperStateChanged = false;
     private void wobbleArm(){
         if (gamepad2.right_trigger > 0){
@@ -118,51 +118,11 @@ public class Main extends LinearOpMode {
             wobbleArmGripperStateChanged = false;
     }
 
-    int enabledDriveControls = 0;
-    private void driveOld(){
-        //get joystick input
-        double joyX = gamepad1.left_stick_x;
-        double joyY = gamepad1.left_stick_y;
-        double joyR = gamepad1.right_stick_x;
-
-        enabledDriveControls = (joyX != 0 || joyY != 0 || joyR != 0) ? 0 : enabledDriveControls;
-        if(enabledDriveControls != 0)
-            return;
-
-        //smooth out the joysticks - f(x) = 0.6x^2 + 0.4x
-//        if(joyX != 0)
-//            joyX = 0.6*(Math.pow(joyX, 2)*(joyX/Math.abs(joyX)))+0.4*joyX;
-//        if(joyY != 0)
-//            joyY = 0.6*(Math.pow(joyY, 2)*(joyY/Math.abs(joyY)))+0.4*joyY;
-//        if(joyR != 0)
-//            joyR = 0.6*(Math.pow(joyR, 2)*(joyR/Math.abs(joyR)))+0.4*joyR;
-
-        joyX *= drivingDirection;
-        joyY *= drivingDirection;
-
-        //reverse y joystick
-        joyY *= -1;
-
-        //create wheel power config
-        WheelPowerConfig wpc = new WheelPowerConfig(
-                joyY + joyX + joyR,
-                joyY - joyX - joyR,
-                joyY + joyX - joyR,
-                joyY - joyX + joyR
-        );
-        wpc.clamp();
-
-        robot.driving.setWheelPowers(wpc);
-    }
     private void drive(){
         //get joystick input
         double forwardInput = gamepad1.left_stick_y*-1;//reversing joystick so up becomes forward
         double strafeInput = gamepad1.right_stick_x;
         double rotationInput = gamepad1.right_trigger - gamepad1.left_trigger;
-
-//        enabledDriveControls = (forwardInput != 0 || strafeInput != 0 || rotationInput != 0) ? 0 : enabledDriveControls;
-//        if(enabledDriveControls != 0)
-//            return;
 
         forwardInput *= drivingDirection;
         strafeInput *= drivingDirection;
@@ -194,40 +154,6 @@ public class Main extends LinearOpMode {
         wpc.clamp();
 
         robot.driving.setWheelPowers(wpc);
-    }
-    private void slowMovement(){
-        enabledDriveControls = (gamepad1.dpad_up || gamepad1.dpad_right || gamepad1.dpad_down || gamepad1.dpad_left || gamepad1.left_bumper || gamepad2.right_bumper) ? 1 : enabledDriveControls;
-        if(enabledDriveControls != 1)
-            return;
-
-        double power = 0.3;
-
-        if (gamepad1.dpad_up) {
-            power *= drivingDirection;
-            WheelPowerConfig wpc = new WheelPowerConfig(power, power, power, power);
-            robot.driving.setWheelPowers(wpc);
-        }else if (gamepad1.dpad_down) {
-            power *= drivingDirection;
-            WheelPowerConfig wpc = new WheelPowerConfig(-power, -power, -power, -power);
-            robot.driving.setWheelPowers(wpc);
-        }else if (gamepad1.dpad_left) {
-            power *= drivingDirection;
-            WheelPowerConfig wpc = new WheelPowerConfig(-1.5*power, 1.5*power, -1.5*power, 1.5*power);
-            robot.driving.setWheelPowers(wpc);
-        }else if (gamepad1.dpad_right){
-            power *= drivingDirection;
-            WheelPowerConfig wpc = new WheelPowerConfig(1.5*power, -1.5*power, 1.5*power, -1.5*power);
-            robot.driving.setWheelPowers(wpc);
-        }else if (gamepad1.left_bumper) {
-            WheelPowerConfig wpc = new WheelPowerConfig(-1.5*power, 1.5*power, 1.5*power, -1.5*power);
-            robot.driving.setWheelPowers(wpc);
-        }else if (gamepad1.right_bumper) {
-            WheelPowerConfig wpc = new WheelPowerConfig(1.5*power, -1.5*power, -1.5*power, 1.5*power);
-            robot.driving.setWheelPowers(wpc);
-        }else {
-            WheelPowerConfig wpc = new WheelPowerConfig(0, 0, 0, 0);
-            robot.driving.setWheelPowers(wpc);
-        }
     }
     boolean drivingDirectionStateChanged = false;
     private void setDrivingDirection(){
