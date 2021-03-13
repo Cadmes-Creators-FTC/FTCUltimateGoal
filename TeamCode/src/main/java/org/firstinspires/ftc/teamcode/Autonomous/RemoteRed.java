770f0c1da04f84481af24176a9f2d6bafde826b2
EmoeBos <58362791+EmoeBos@users.noreply.github.com>
2021-03-13 17:42:52 +0100

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RemoteRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RemoteRed.java
@@ -39,25 +39,42 @@ public class RemoteRed extends LinearOpMode {
 
     //autonomous sequence
     private void autonomousSequence() throws InterruptedException {
-        // put arm in autonomous up pos to extend arm
-        robot.wobbleArm.armUpAutonomous();
 
-        robot.driving.driveToPositionForwardOnly(new Vector2(200, 170), 45.0, 0.75);
-        robot.wobbleArm.armDownAutonomous();
-        robot.wobbleArm.openGripperAutonomous();
+        double numOfRings = robot.ringStackDetection.getStackSize();
+        robot.logging.setLog("numOfRings", numOfRings);
+        Vector2 wobbleDropPoint = new Vector2(200, 170);
+        if(numOfRings == 1)
+            wobbleDropPoint = new Vector2(140, 230);
+        else if(numOfRings == 4)
+            wobbleDropPoint = new Vector2(200, 290);
 
-//        robot.driving.driveToPosition(new Vector2(110, 90), 180.0, 1);
-////        robot.driving.driveToPositionForwardOnly(new Vector2(110, 90), 180.0, 1);
+        robot.wobbleArm.armDownAutonomous(2500);
+        robot.wobbleArm.armUpAutonomous(500);
+        //drop off first wobble goal
+        robot.driving.driveToPositionForwardOnly(new Vector2(210, 120), null, 0.75);
+        robot.driving.driveToPositionForwardOnly(wobbleDropPoint, 30.0, 0.75);
+        robot.wobbleArm.armDownAutonomous(400);
+        robot.wobbleArm.openGripperAutonomous(400);
+
+        //shoot rings
+        robot.driving.driveToPositionForwardOnly(new Vector2(150, 180), 180.0, 0.75);
+        robot.shooter.turnOn(0.92);
+        robot.conveyor.turnOn(1);
+        Thread.sleep(1000);
+        robot.shooter.turnOff();
+        robot.conveyor.turnOff();
+
+//        //pick up second wobble goal
+//        robot.driving.driveToPositionForwardOnly(new Vector2(130, 100), null, 0.75);
 //        robot.wobbleArm.closeGripperAutonomous();
 //        robot.wobbleArm.armUpAutonomous();
 //
-//        robot.driving.driveToPosition(new Vector2(180, 190), 45.0, 1);
-////        robot.driving.driveToPositionForwardOnly(new Vector2(180, 190), 45.0, 1);
-//        robot.wobbleArm.armDownAutonomous();
+//        //drop off second wobble goal
+//        robot.driving.driveToPositionForwardOnly(new Vector2(200, 170), null, 0.75);
 //        robot.wobbleArm.openGripperAutonomous();
-//        robot.wobbleArm.armUpAutonomous();
-//
-//        robot.driving.driveToPosition(new Vector2(120, 210), 45.0, 1);
-////        robot.driving.driveToPositionForwardOnly(new Vector2(120, 210), 45.0, 1);
+//        robot.wobbleArm.toStopPosition();
+
+        //drive to launch line
+        robot.driving.driveToPositionForwardOnly(new Vector2(100, 210), null, 0.75);
     }
 }
