a4278b1476fd5caea697751ec079f5ec2fba9e99
EmoeBos <58362791+EmoeBos@users.noreply.github.com>
2021-03-17 22:29:23 +0100

    ring detectie
    waardes morgen controleren of ze goed staan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RingStackDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RingStackDetection.java
@@ -39,8 +39,8 @@ public class RingStackDetection extends RobotComponent {
     public int getStackSize(){
         int avgRedVal = camPipeline.getAvgRedVal();
 
-        int oneRingThreshhold = 127;
-        int fourRingThreshhold = 145;
+        int oneRingThreshhold = 132;//127
+        int fourRingThreshhold = 140;//145
 
         int stackSize = 0;
         if(avgRedVal > fourRingThreshhold)
