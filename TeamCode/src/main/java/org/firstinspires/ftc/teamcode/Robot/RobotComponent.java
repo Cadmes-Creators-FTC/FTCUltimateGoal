9fc30eefa346416c9d7c068a5fd597df3b34388a
TBchip <bischoffthijs@gmail.com>
2021-05-23 12:58:49 +0200

    removed comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotComponent.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class RobotComponent {
-    public final MainRobot robot; //reference to robot
+    public final MainRobot robot;
 
     public RobotComponent(MainRobot inputRobot) {
         robot = inputRobot;
